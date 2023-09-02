5cfb6c38ce5213228c76001f3054d2fc040de3a7
CamillyFernandes <fcamilly129@gmail.com>
2023-09-01 22:51:16 -0300

    DiceRoller

---
+++ MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace DiceRoller;
+using System;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+    private void RolaroDado(object sender, EventArgs e)
+    {
+        Random random = new Random();
+        resultado.Text = Convert.ToString (random.Next(1, 7));
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+
+
+       
+    }
+}
+
